b9ad83ba173c8fb13e6e5863bcba5fb38d8fa2fe
hblee12 <hbllee@ucdavis.edu>
2017-01-16 19:28:55 -0800

    Initial commit
    -changed code again

--- studio1/script.js
+++ studio1/script.js
@@ -4,11 +4,11 @@
 // like print() messages in Processing
 console.log("this is a js comment");
 
-var img;
-function preload(){
-    img = loadImage("banner_icons_1.png");
-}
 
+var img;
+function preload() {
+  img = loadImage("banner_icons_1.png");
+}
 function setup() {
   image(img, 0, 0);
 }
