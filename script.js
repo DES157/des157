81319ae54593a050b777c42a6a1ae4dd9a6dad0a
annnh <anhla9@scc2102-20.ucdavis.edu>
2017-01-17 09:38:26 -0800

    moved some stuff
    moved studio1 content to main directory

---
+++ script.js
@@ -0,0 +1,39 @@
+// p5 file
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log("this is a js comment");
+
+var x = 0;  //rotation increments start
+var s = 60; // size of square
+var o = 0;  //origin
+
+function setup() {
+  //create a variable that references the html5 canvas
+  //remember, what was called size() in Processing is called
+  //createCanvas() is p5.js
+  var myCanvas = createCanvas(800,250);
+
+  //parent the myCanvas variable to the html element titled "mySketch"
+  myCanvas.parent('mySketch');
+}
+
+function draw() {
+  background(255);
+  translate(width/2, height/2);
+  rotate(radians(0+x));
+
+  noStroke();
+  
+  fill(234, 195, 36);
+  rectMode(CENTER);
+  rect(0, 0, s, s);
+}
+
+function mouseReleased() {
+  if (mouseButton == LEFT) {
+    x+=10*PI;
+  } else {
+    x-=10*PI;
+  }
+}
