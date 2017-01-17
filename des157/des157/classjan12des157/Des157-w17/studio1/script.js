ccd39c0eedabfac58ba5a52cf7c35afa7865c6ca
kstor001 <kastormer@ucdavis.edu>
2017-01-17 09:58:01 -0800

    Add files via upload

--- des157/des157/classjan12des157/Des157-w17/studio1/script.js
+++ des157/des157/classjan12des157/Des157-w17/studio1/script.js
@@ -1,6 +1,8 @@
 function setup() {
   var myCanvas = createCanvas(800,250);
-  myCanvas.parent ('mySketch');  
+  myCanvas.parent ('mySketch');
+  
+
 }
 
 function draw() {
@@ -13,9 +15,9 @@ function draw() {
   color[] colors = new color[4];
 //lines color ranges array
  colors[0] = color(255);
- colors[1] = color('#5AC6D3');
- colors[2] = color('#35A8AC');
- colors[3] = color('#125A6D');
+ colors[1] = color(#F0DDB2);
+ colors[2] = color(#DFB3C0);
+ colors[3] = color(#C4A4CC);
  
   stroke(colors[var(random(colors.length))]);
   strokeWeight(2);
@@ -26,7 +28,7 @@ function draw() {
   
  if (touchX <=200){
    strokeWeight(10);
-   stroke('#5AC6D3');
+   stroke(#F0DDB2);
    noFill();
    ellipse(k,j,p,p);
    
@@ -34,7 +36,7 @@ function draw() {
    
  } else if ((touchX >200)&&(touchX <400)){
    strokeWeight(10);
-   stroke('#35A8AC');
+   stroke(#DFB3C0);
    noFill();
    ellipse(k,j,p,p);
   
@@ -48,7 +50,7 @@ function draw() {
    
  } else{
    strokeWeight(10);
-   stroke('#125A6D');
+   stroke(#C4A4CC);
    noFill();
    ellipse(k,j,p,p);
   
