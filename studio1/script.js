eb9b7e8aa1795df2f08a58e754e4d85bec9fad6d
hblee12 <hbllee@ucdavis.edu>
2017-01-16 18:26:37 -0800

    Initial commit
    -added header code
    -banner icons

---
+++ studio1/script.js
@@ -0,0 +1,14 @@
+// p5 file
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log("this is a js comment");
+
+var img;
+function preload(){
+    img = loadImage("banner_icons_1.png");
+}
+
+function setup() {
+  image(img, 0, 0);
+}
