df9f5e455a3f775b5d881dfcf6543be161cf8069
kstor001 <kastormer@ucdavis.edu>
2017-01-24 21:23:18 -0800

    Add files via upload

--- des157/script.js
+++ des157/script.js
@@ -1,69 +1,70 @@
-  var colorIndex;
-  var colors;
- 
- function setup() {
-  var myCanvas = createCanvas(800,250);
-  myCanvas.parent ('mySketch');  
-  colors = [color('#fff'), color('#4391AF'), color('#52BDE7'), color('#4786A2')];
- 
+var colorIndex;
+var colors;
+
+function setup() {
+var myCanvas = createCanvas(800,250);
+myCanvas.parent ('mySketch');
+colors = [color('#fff'), color('#4391AF'), color('#52BDE7'), color('#4786A2')];
+
 }
 
 
 
 function draw() {
-  noFill();
-  stroke(255);
-  var k= 400;
-  var j= 125;
-  var p= 100;
-  var q= random(300,500);
- 
+noFill();
+stroke(255);
+var k= 400;
+var j= 125;
+var p= 100;
+var q= random(300,500);
+
 //lines color ranges array
- colorIndex=int(random(colors.length-1));
+colorIndex=int(random(colors.length-1));
 
 console.log('colorIndex: ' + colorIndex);
- 
+
 stroke(255);
-  strokeWeight(2);
-  ellipse(k,j,p,p);
-    stroke(colors[colorIndex]);
-  line(k,j,touchX,touchY);
- 
- 
-//circle color changes based on mouse   
-  
- if (touchX <=200){
-   strokeWeight(10);
-   stroke('#4391AF');
-   noFill();
-   ellipse(k,j,p,p);
-   
+strokeWeight(60);
+ellipse(k,j,p,p);
+  stroke(colors[colorIndex]);
+line(k,j,touchX,touchY);
 
-   
- } else if ((touchX >200)&&(touchX <400)){
-   strokeWeight(10);
-   stroke('#52BDE7');
-   noFill();
-   ellipse(k,j,p,p);
-  
-  
- } else if ((touchX >400)&&(touchX <600)){
-   strokeWeight(10);
-   stroke(255);
-   noFill();
-   ellipse(k,j,p,p);
-  
-   
- } else{
-   strokeWeight(10);
-   stroke('#4786A2');
-   noFill();
-   ellipse(k,j,p,p);
-  
 
- }
- 
-    
+//circle color changes based on mouse
+//lines below
+if (touchX <=200){
+ strokeWeight(40);
+ stroke('#4391AF');
+ noFill();
+ ellipse(k,j,p,p);
+
+
+
+} else if ((touchX >200)&&(touchX <400)){
+ strokeCap(PROJECT);
+ strokeWeight(90);
+ stroke('#52BDE7');
+ noFill();
+ ellipse(k,j,p,p);
+
+
+} else if ((touchX >400)&&(touchX <600)){
+ strokeCap(PROJECT);
+ strokeWeight(30);
+ stroke(255);
+ noFill();
+ ellipse(k,j,p,p);
+
+
+} else{
+ strokeCap(PROJECT);
+ strokeWeight(50);
+ stroke('#4786A2');
+ noFill();
+ ellipse(k,j,p,p);
+
+
 }
- 
- 
+
+
+}
