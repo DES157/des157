9c07a73499cd3e311c2784016c1a3dea31ccc73d
9hours <ahalam@ucdavis.edu>
2017-01-31 05:03:32 -0800

    studio2
    gotta do more bleh

--- studio2/script.js
+++ studio2/script.js
@@ -37,17 +37,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //if all entries ARE NOT FILLED, display alert
     if (name1=="" || name2=="" || noun1=="" || noun2=="" || noun3=="" || noun4=="" || adj1=="" || adj2=="" || adj3=="" || adj4=="" || adj5=="") {
-      alert("BLEH!");
-    }
+      alert("I have to finish this letter before I can send it!");
+    } //end of if statement
 
     //if all entries are filled, display myMsg
     else {
+
     //target the html element
-    var myMsg = document.getElementById("myMsg");
+    var bgimg = document.getElementById("bgimg"); //HIDING AND REVEALING THE IMG
+    var myMsg = document.getElementById("myMsg"); //msg inside aside
 
-    myMsg.innerHTML="My Dearest, " + name1 + ". Please excuse the state of my handwriting for I am still in a state of " + noun1 + " and " + noun2 + " from being in your presence. It is with great pleasure to finally express my " + noun3 + " to you. I hope this does not come as a surprise from our daily walks that I find you utterly " + adj1 + " So please, do forgive me for resorting to such quieter means of confession. Forgive me for my " + noun4 + ", as I cannot face you in my current condition. I am but a " + adj2 + "  fool! A fool for your love and " + adj3 + " disposition and" + adj4 + " smile, oh, I sound like a " + adj5 + " fool. Please, would you kindly keep me in your thoughts as well? I hope my boldness does not deter you and I look forward to our next meeting. Forever yours, " + name2;
+    myMsg.innerHTML="<div>My Dearest, <em>" + name1 + "</em></div> Please excuse the state of my handwriting for I am still in a state of <em>" + noun1 + "</em> and <em>" + noun2 + "</em> from being in your presence. It is with great pleasure to finally express my <em>" + noun3 + "</em> to you. I hope this does not come as a surprise from our daily walks that I find you utterly <em>" + adj1 + "</em>. So please, do forgive me for resorting to such quieter means of confession. Forgive me for my <em>" + noun4 + "</em>, as I cannot face you in my current condition. I am but a <em>" + adj2 + "</em> fool! A fool for your love and <em>" + adj3 + "</em> disposition and <em>" + adj4 + "</em> smile, oh, I sound like a <em>" + adj5 + "</em> fool. Please, would you kindly keep me in your thoughts as well? I hope my boldness does not deter you and I look forward to our next meeting. <div>Forever yours, <em>" + name2 + "</em></div>";
 
-    myMsg.className="show"
+    bgimg.className="show"; //select p class name and change so you can see the message
     }
 
     //prevent page from reloading
@@ -57,6 +59,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //clear the form on reset
     function clearForm() {
       myMsg.innerHTML="";
-      myMsg.className="hide";
+      bgimg.className="hide"; //change name to hide
     }
 });
